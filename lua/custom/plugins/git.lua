b9eef9383854356e5fc98d1922bfe78ff7258be8
truelzsch <130214299+truelzsch@users.noreply.github.com>
2025-01-26 19:55:31 +0100

    Lualines (#2)
    * Config Bubbles design
    
    * Add git git
    
    * Update file layout

---
+++ lua/custom/plugins/git.lua
@@ -0,0 +1,142 @@
+-- Plugins: Git
+-- https://github.com/rafi/vim-config
+
+local has_git = vim.fn.executable 'git' == 1
+
+return {
+
+  -----------------------------------------------------------------------------
+  -- Tabpage interface for cycling through diffs
+  {
+    'sindrets/diffview.nvim',
+    cond = has_git,
+    cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
+    keys = {
+      { '<leader>gd', '<cmd>DiffviewFileHistory %<CR>', desc = 'Diff File' },
+      { '<leader>gv', '<cmd>DiffviewOpen<CR>', desc = 'Diff View' },
+    },
+    opts = function()
+      local actions = require 'diffview.actions'
+      vim.api.nvim_create_autocmd({ 'WinEnter', 'BufEnter' }, {
+        group = vim.api.nvim_create_augroup('rafi.diffview', {}),
+        pattern = 'diffview:///panels/*',
+        callback = function()
+          vim.opt_local.cursorline = true
+          vim.opt_local.winhighlight = 'CursorLine:WildMenu'
+        end,
+      })
+
+      return {
+        enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
+        keymaps = {
+          view = {
+            { 'n', 'q', actions.close },
+            { 'n', '<Tab>', actions.select_next_entry },
+            { 'n', '<S-Tab>', actions.select_prev_entry },
+            { 'n', '<localleader>a', actions.focus_files },
+            { 'n', '<localleader>e', actions.toggle_files },
+          },
+          file_panel = {
+            { 'n', 'q', actions.close },
+            { 'n', 'h', actions.prev_entry },
+            { 'n', 'o', actions.focus_entry },
+            { 'n', 'gf', actions.goto_file },
+            { 'n', 'sg', actions.goto_file_split },
+            { 'n', 'st', actions.goto_file_tab },
+            { 'n', '<C-r>', actions.refresh_files },
+            { 'n', '<localleader>e', actions.toggle_files },
+          },
+          file_history_panel = {
+            { 'n', 'q', '<cmd>DiffviewClose<CR>' },
+            { 'n', 'o', actions.focus_entry },
+            { 'n', 'O', actions.options },
+          },
+        },
+      }
+    end,
+  },
+
+  -----------------------------------------------------------------------------
+  -- Magit clone for Neovim
+  {
+    'NeogitOrg/neogit',
+    cond = has_git,
+    cmd = 'Neogit',
+    keys = {
+      { '<leader>mg', '<cmd>Neogit<CR>', desc = 'Neogit' },
+    },
+    -- See: https://github.com/TimUntersberger/neogit#configuration
+    opts = {
+      disable_signs = false,
+      disable_context_highlighting = false,
+      disable_commit_confirmation = false,
+      signs = {
+        section = { '>', 'v' },
+        item = { '>', 'v' },
+        hunk = { '', '' },
+      },
+      integrations = {
+        diffview = true,
+      },
+    },
+  },
+
+  -----------------------------------------------------------------------------
+  -- Git blame visualizer
+  {
+    'FabijanZulj/blame.nvim',
+    cond = has_git,
+    cmd = 'ToggleBlame',
+		-- stylua: ignore
+		keys = {
+			{ '<leader>gb', '<cmd>BlameToggle virtual<CR>', desc = 'Git blame' },
+			{ '<leader>gB', '<cmd>BlameToggle window<CR>', desc = 'Git blame (window)' },
+		},
+    opts = {
+      date_format = '%Y-%m-%d %H:%M',
+      merge_consecutive = false,
+      max_summary_width = 30,
+      mappings = {
+        commit_info = 'K',
+        stack_push = '>',
+        stack_pop = '<',
+        show_commit = '<CR>',
+        close = { '<Esc>', 'q' },
+      },
+    },
+  },
+
+  -----------------------------------------------------------------------------
+  -- Pleasant editing on Git commit messages
+  {
+    'rhysd/committia.vim',
+    cond = has_git,
+    event = 'BufReadPre COMMIT_EDITMSG',
+    init = function()
+      -- See: https://github.com/rhysd/committia.vim#variables
+      vim.g.committia_min_window_width = 30
+      vim.g.committia_edit_window_width = 75
+    end,
+    config = function()
+      vim.g.committia_hooks = {
+        edit_open = function()
+          vim.cmd.resize(10)
+          local opts = {
+            buffer = vim.api.nvim_get_current_buf(),
+            silent = true,
+          }
+          local function map(mode, lhs, rhs)
+            vim.keymap.set(mode, lhs, rhs, opts)
+          end
+          map('n', 'q', '<cmd>quit<CR>')
+          map('i', '<C-d>', '<Plug>(committia-scroll-diff-down-half)')
+          map('i', '<C-u>', '<Plug>(committia-scroll-diff-up-half)')
+          map('i', '<C-f>', '<Plug>(committia-scroll-diff-down-page)')
+          map('i', '<C-b>', '<Plug>(committia-scroll-diff-up-page)')
+          map('i', '<C-j>', '<Plug>(committia-scroll-diff-down)')
+          map('i', '<C-k>', '<Plug>(committia-scroll-diff-up)')
+        end,
+      }
+    end,
+  },
+}
--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -2,60 +2,60 @@
 -- https://github.com/rafi/vim-config
 
 return {
-	{
-		'folke/snacks.nvim',
-		opts = {
-			dashboard = { enabled = false },
-			scroll = { enabled = false },
-			terminal = {
-				win = { style = 'terminal', wo = { winbar = '' } },
-			},
-			zen = {
-				toggles = { git_signs = true },
-				zoom = {
-					show = { tabline = false },
-					win = { backdrop = true },
-				},
-			},
-		},
-		keys = {
-			{
-				'<leader>N',
-				desc = 'Neovim News',
-				function()
-					---@diagnostic disable-next-line: missing-fields
-					Snacks.win({
-						file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
-						width = 0.6,
-						height = 0.6,
-						wo = {
-							spell = false,
-							wrap = false,
-							signcolumn = 'yes',
-							statuscolumn = ' ',
-							conceallevel = 3,
-						},
-					})
-				end,
-			},
-		},
-	},
+  {
+    'folke/snacks.nvim',
+    opts = {
+      dashboard = { enabled = false },
+      scroll = { enabled = false },
+      terminal = {
+        win = { style = 'terminal', wo = { winbar = '' } },
+      },
+      zen = {
+        toggles = { git_signs = true },
+        zoom = {
+          show = { tabline = false },
+          win = { backdrop = true },
+        },
+      },
+    },
+    keys = {
+      {
+        '<leader>N',
+        desc = 'Neovim News',
+        function()
+          ---@diagnostic disable-next-line: missing-fields
+          Snacks.win {
+            file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
+            width = 0.6,
+            height = 0.6,
+            wo = {
+              spell = false,
+              wrap = false,
+              signcolumn = 'yes',
+              statuscolumn = ' ',
+              conceallevel = 3,
+            },
+          }
+        end,
+      },
+    },
+  },
 
-	-----------------------------------------------------------------------------
-	-- Hint and fix deviating indentation
-	{
-		'tenxsoydev/tabs-vs-spaces.nvim',
-		event = { 'BufReadPost', 'BufNewFile' },
-		opts = {},
-	},
+  -----------------------------------------------------------------------------
+  -- Hint and fix deviating indentation
+  {
+    'tenxsoydev/tabs-vs-spaces.nvim',
+    event = { 'BufReadPost', 'BufNewFile' },
+    opts = {},
+  },
 
-	-----------------------------------------------------------------------------
-	-- Highlight words quickly
-	{
-		't9md/vim-quickhl',
+  -----------------------------------------------------------------------------
+  -- Highlight words quickly
+  {
+    't9md/vim-quickhl',
 		-- stylua: ignore
 		keys = {
 			{ '<leader>mt', '<Plug>(quickhl-manual-this)', mode = { 'n', 'x' }, desc = 'Highlight word' },
 		},
-	},
+  },
 }
