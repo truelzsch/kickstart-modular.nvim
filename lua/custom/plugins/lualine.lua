94e207fc0bdafa1e4309650f958949135f414390
Marco Trülzsch <marco.truelzsch@t-online.de>
2025-01-26 13:50:21 +0100

    Add simple lualine

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,14 @@
+-- Plugin: Lualine
+-- https://github.com/rafi/vim-config
+
+return {
+
+  -----------------------------------------------------------------------------
+  -- Statusline plugin with many customizations.
+  -- NOTE: This extends
+  -- $XDG_DATA_HOME/nvim/lazy/LazyVim/lua/lazyvim/plugins/ui.lua
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+}
